129ba85b406788fc550a840492ed57875971d92a
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2024-03-22 14:49:56 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 const CACHE_NAME = 'cool-cache';
 
 // Add whichever assets you want to pre-cache here:
@@ -16,3 +17,21 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
+self.addEventListener('fetch', event => {
+    event.respondWith(async () => {
+        const cache = await caches.open(CACHE_NAME);
+  
+        // match the request to our cache
+        const cachedResponse = await cache.match(event.request);
+  
+        // check if we got a valid response
+        if (cachedResponse !== undefined) {
+            // Cache hit, return the resource
+            return cachedResponse;
+        } else {
+          // Otherwise, go to the network
+            return fetch(event.request)
+        }
+    });
+  });
+  
