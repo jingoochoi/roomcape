149cc3ff17b565a8875bad10c8ed52819880aee1
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2024-03-22 12:35:31 +0900

    .

--- src/module/Room1.jsx
+++ src/module/Room1.jsx
@@ -1,7 +1,10 @@
+import '../css/room.css'
 export function Room1() {
     return(
         <>
-            <div className="door"></div>
+            <div className="door">
+                <div className="hand"></div>
+            </div>
         </>
     )
 }
