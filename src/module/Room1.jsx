0a028f3af65ecd65a80581f2b988b63b4ebaed5e
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2024-03-23 12:07:55 +0900

    .

--- src/module/Room1.jsx
+++ src/module/Room1.jsx
@@ -2,7 +2,7 @@ import '../css/room.css'
 export function Room1() {
     return(
         <>
-            <div className="door">
+            <div className="door woodleft">
                 <div className="hand"></div>
             </div>
         </>
--- src/module/Room2.jsx
+++ src/module/Room2.jsx
@@ -2,7 +2,14 @@ import '../css/room.css'
 export function Room2() {
     return(
         <>
-            <div className="draw"></div>
+            <div className="draw wood">
+                <div className="pock up wooddown">
+                    <div className="open wood"></div>
+                </div>
+                <div className="pock down wooddown">
+                    <div className="open wood"></div>
+                </div>
+            </div>
         </>
     )
 }
