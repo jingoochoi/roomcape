320c3060f8ac87bdba2f097199dd12f8eafe8d43
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2024-03-26 11:10:19 +0900

    .

--- src/module/Room4.jsx
+++ src/module/Room4.jsx
@@ -2,7 +2,7 @@ import '../css/room.css'
 export function Room4() {
     return(
         <>
-            <div className="poto">🌬</div>
+            <div className="poto">❓</div>
         </>
     )
 }
