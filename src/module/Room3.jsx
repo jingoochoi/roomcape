0a09778eb91a6b5895dd03aaaba35be9ad500bf6
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2024-03-27 12:19:31 +0900

    .

--- src/module/Room3.jsx
+++ src/module/Room3.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react'
 import '../css/room.css'
+// import $ from 'jquery'
 export function Room3() {
+    useEffect(()=>{
+        
+    },[])
     return(
         <>
-            <div className="wind">🌆</div>
+            <div className="wind">🌆
+                <div className="none"></div>
+            </div>
             <div className="hole"></div>
         </>
     )
