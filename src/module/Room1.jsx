cd141a488df8c35925b2ae82a45a853fd1412976
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2024-03-30 10:29:44 +0900

    .

--- src/module/Room1.jsx
+++ src/module/Room1.jsx
@@ -1,9 +1,28 @@
 import '../css/room.css'
+import $ from 'jquery'
 export function Room1() {
+    const rota=()=>{
+        const hintElement = document.querySelector('.sult');
+        if ($('.poto').text()==='🔑') {
+            hintElement.textContent = 'LOCKED!!';
+            hintElement.style.display = 'block';
+            setTimeout(() => {
+                hintElement.style.display='none'
+            }, 1000);
+        } else if ($('.poto').text()==='') {
+            // Make sure coco.cod1 is defined and accessible
+            // alert('oo')
+            $('#root').fadeOut(1000)
+            setTimeout(() => {
+                window.close()
+            }, 1300);
+        }
+    }
     return(
         <>
+            <div className="sult" style={{position:'absolute',width:'200px',height:'130px',display:'none',zIndex:333,backgroundColor:'white',borderRadius:'30px',lineHeight:7}}></div>
             <div className="door woodleft">
-                <div className="hand"></div>
+                <div className="hand" onClick={rota}></div>
             </div>
         </>
     )
